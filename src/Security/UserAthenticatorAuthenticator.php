803241f173027093516d66cf12d75012e01e99b2
anis brahmi <anisbrahmi840@gmail.com>
2021-03-09 03:33:27 +0100

    auth entrepreneur&admin

--- src/Security/UserAthenticatorAuthenticator.php
+++ src/Security/UserAthenticatorAuthenticator.php
@@ -24,7 +24,7 @@ class UserAthenticatorAuthenticator extends AbstractFormLoginAuthenticator imple
 {
     use TargetPathTrait;
 
-    public const LOGIN_ROUTE = 'app_login';
+    public const LOGIN_ROUTE = 'users_login';
 
     private $entityManager;
     private $urlGenerator;
